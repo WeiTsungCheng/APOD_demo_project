f78d310cf1c787e4c321a89910cc60c7e9b6b636
WEI-TSUNG CHENG <3118197C@student.gla.ac.uk>
2026-04-25 20:08:49 +0100

    Initial Commit

---
+++ APOD_demo_project/APOD_demo_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  APOD_demo_projectApp.swift
+//  APOD_demo_project
+//
+//  Created by WEITSUNG on 25/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct APOD_demo_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
