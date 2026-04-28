117115257e0f30fad1321307f4272ab732f694c9
WEI-TSUNG CHENG <3118197C@student.gla.ac.uk>
2026-04-28 12:18:30 +0100

    refactor: extra contentView from APODDayView

--- APOD_demo_project/Screen/APODDayView.swift
+++ APOD_demo_project/Screen/APODDayView.swift
@@ -17,41 +17,81 @@ struct APODDayView: View {
     
     var body: some View {
         NavigationStack {
+            
             Group {
-                if let apod = vm.apod {
+                if vm.isLoading {
+                    ProgressView()
                     
-                    if apod.mediaType == .image {
-                        AsyncImage(url: apod.url) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView()
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                            case .failure:
-                                Image(systemName: "photo")
-                            @unknown default:
-                                EmptyView()
-                            }
-                        }
-                        .frame(width: 200, height: 200)
-                        
-                    } else {
-                        WebVideoView(url: apod.url)
-                            .frame(height: 300)
-                    }
+                } else if vm.apod != nil {
+                    contentView
+                    
+                } else if let errorMessage = vm.errorMessage {
+                    Text(errorMessage)
                     
                 } else {
-                    ProgressView()
+                    Text("No APOD loaded")
                 }
-                
             }.task {
-//                await vm.loadAPOD(date: Date.fromAPODString("2021-10-10"))
-//                await vm.loadAPOD(date: Date().addingDays(-1))
+                //                await vm.loadAPOD(date: Date.fromAPODString("2021-10-10"))
+                //                await vm.loadAPOD(date: Date().addingDays(-1))
                 await vm.loadAPOD(date: Date())
             }
         }
+    }
+}
+
+extension APODDayView {
+    
+    @ViewBuilder var mediaView: some View {
+        if let apod = vm.apod {
+            let url = apod.url
+            let type = apod.mediaType
+            
+            if type == .image {
+                AsyncImage(url: url) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    case .failure:
+                        Image(systemName: "photo")
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+                
+            } else {
+                WebVideoView(url: url)
+            }
+        }
+    }
+    
+    @ViewBuilder var contentView: some View {
+        if let apod = vm.apod {
+            
+            ScrollView {
+                Text(apod.title)
+                    .font(.title2)
+                    .bold()
+                
+                mediaView
+                    .frame(height: 300)
+                
+                Text(apod.explanation)
+                    .font(.body)
+                
+                if let copyright = apod.copyright {
+                    Text("@ \(copyright)")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        }
+        
     }
 }
 
